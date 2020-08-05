146b245e8fdc412abd7df1a10d744489769591d1
Seoungwoo Baek <seobaek@c2r7s3.42seoul.kr>
2020-08-05 16:34:45 +0900

    baekjoon

---
+++ baekjoon/42Algo/16917.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+	size_t yang;
+	size_t fri;
+	size_t half;
+	size_t yang_vol;
+	size_t fri_vol;
+	size_t res;
+
+	res = 0;
+	scanf("%zd %zd %zd %zd %zd", &yang, &fri, &half, &yang_vol, &fri_vol);
+	if (yang > fri && yang > half && fri > half)
+	{
+		if (yang_vol > fri_vol)
+			res = (2 *  * ) +
+	}
+}
