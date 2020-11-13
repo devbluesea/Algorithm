8429403cb329b053d1c41f0ab311de3c61aebd22
tamabii <seobaek@student.42seoul.kr>
2020-11-13 13:45:38 +0900

    Add files via upload

--- baekjoon/Step/BruteForce/1018.c
+++ baekjoon/Step/BruteForce/1018.c
@@ -1,26 +1,84 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int	main(void)
+int	check_board(int y, int x, char chess[][50])
 {
-	int	num_n;
-	int	num_m;
-	int	count;
-	int	i;
-	int	j;
-	char	**chess;
+	int i;
+	int j;
+	int fix_w;
+	int fix_b;
 
-	scanf("%d %d", &num_n, &num_m);
-	chess = malloc(sizeof(char) * num_n);
-	for (i = 0 ; i < num_n ; i++)
+	fix_w = 0;
+	fix_b = 0;
+	i = 0;
+	while (i < 8)
 	{
-		chess[i] = malloc(sizeof(char) * num_m);
+		j = 0;
+		while (j < 8)
+		{
+			if (i % 2 == 0)
+			{
+				if (j % 2 == 0 && chess[y + i][x + j] != 'W')
+					fix_w++;
+				if (j % 2 == 0 && chess[y + i][x + j] != 'B')
+					fix_b++;
+				if (j % 2 == 1 && chess[y + i][x + j] == 'W')
+					fix_w++;
+				if (j % 2 == 1 && chess[y + i][x + j] == 'B')
+					fix_b++;
+			}
+			if (i % 2 == 1)
+			{
+				if (j % 2 == 0 && chess[y + i][x + j] == 'W')
+					fix_w++;
+				if (j % 2 == 0 && chess[y + i][x + j] == 'B')
+					fix_b++;
+				if (j % 2 == 1 && chess[y + i][x + j] != 'W')
+					fix_w++;
+				if (j % 2 == 1 && chess[y + i][x + j] != 'B')
+					fix_b++;
+			}
+			j++;
+		}
+		i++;
+	}
+	return (fix_b < fix_w ? fix_b : fix_w);
+}
+
+int main(void)
+{
+	int i;
+	int j;
+	int width;
+	int height;
+	int y_count;
+	int x_count;
+	int fix;
+	int fix_min;
+	char chess[50][50];
+
+	i = 0;
+	scanf("%d %d", &height, &width);
+	for(i = 0 ; i < height ; i++)
 		scanf("%s", chess[i]);
-	}
-	for (i = 0 ; i < num_n ; i++)
+	*chess[i] = '\0';
+	i = 0;
+	fix_min = 64;
+	y_count = height - 8;
+	while (y_count >= 0)
 	{
-		printf("%s", chess[i]);
-		printf("\n");
+		j = 0;
+		x_count = width - 8;
+		while (x_count >= 0)
+		{
+			fix = check_board(i, j, chess);
+			if (fix < fix_min)
+				fix_min = fix;
+			j++;
+			x_count--;
+		}
+		i++;
+		y_count--;
 	}
+	printf("%d\n", fix_min);
 	return (0);
 }
